9de56fb56edcd1f56816c5858046ff36463647c9
rui775 <rsilva6420.dev@gmail.com>
2024-08-20 15:59:23 +0100

    Add handlebars. Finish

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
+const templateElement = document.getElementById("templateHB");
+const templateSource = templateElement.innerHTML;
+const template = Handlebars.compile(templateSource);
+
 const context = {
     title: 'Welcome to Musicon',
     body: 'Musicon is a budding musical storefront with a mission to share the joy of music. These magnificent auditory tools are designed with musical creators, like you, in mind. Hobbyists, beginners, and experts alike can appreciate the resplendent sounds supplied by each and every instrument we carry. Join us in delivering the euphoric vibrations of melodia to the citizens of the world!',
@@ -27,6 +31,15 @@ const context = {
             description: 'Ever thought, "one instrument isn\'t enough?" Find an answer in the drum kit. Coordinate a collections of drums and cymbals to dictate the rhythm of musical masterpiece.',
             price: '$649.00',
             sale: '$349.00'
+        },
+        {
+            image: 'https://content.codecademy.com/courses/learn-handlebars/musicon/violin.png',
+            name: 'Violin',
+            description: 'A versatile instrument that is suited for any and all occasions. Those wearing tuxedos can strum together a classic. Others who prefer overalls can call it a fiddle and play some folk songs.',
+            price: '$245.00'
         }
     ]
 };
+
+const compiledHtml = template(context);
+document.getElementById("information").innerHTML = compiledHtml;
